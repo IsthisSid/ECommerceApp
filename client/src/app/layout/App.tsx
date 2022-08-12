5624593f1e5a9c01fc3a700a35f82a44846d2cbc
Sidany Walker <sidanyk@gmail.com>
2022-08-11 20:20:04 -0400

    set up React Router; add nav links
    Place <App /> inside <BrowserRouter> tags in the index.tsx file. In App.tsx, replace <Catalog /> with Route paths to different pages: Home, Catalog, ProductDetails, About, and Contact Pages. Set up nav links to those pages

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 function App() {
@@ -25,8 +30,13 @@ function handleThemeChange() {
     <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-      <Catalog />
-      </Container>
+<Route exact path='/' component={HomePage}/>
+<Route exact path='/catalog' component={Catalog}/>      
+<Route path='/catalog/:id' component={ProductDetails}/>      
+<Route path='/about' component={AboutPage}/>      
+<Route path='/contact' component={ContactPage}/>      
+
+</Container>
     </ThemeProvider>
   );
 }
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,9 +1,20 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'},
+]
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'},
+]
 
 export default function Header({darkMode, handleThemeChange}: Props) {
     return (
@@ -13,6 +24,27 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                     The Random Shop
                 </Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange}/>
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem component={NavLink}
+                        to={path}
+                        key={path}
+                        sx={{color: 'inherit', typography: 'h6'}}>
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                <List sx={{display: 'flex'}}>
+                    {rightLinks.map(({title, path}) => (
+                        <ListItem 
+                        component={NavLink}
+                        to={path}
+                        key={path}
+                        sx={{color: 'inherit', typography: 'h6'}}>
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
             </Toolbar>
         </AppBar>
     )
