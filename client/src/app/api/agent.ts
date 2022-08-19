ccdbfbca762de011ec29d19faf7b7c94db1f4af8
Sidany Walker <sidanyk@gmail.com>
2022-08-19 11:43:45 -0400

    Handle and display validation errors

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -11,6 +11,15 @@ axios.interceptors.response.use(response => {
     const {data, status} = error.response as any;
     switch (status) {
         case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
             toast.error(data.title);
             break;
         case 401:
