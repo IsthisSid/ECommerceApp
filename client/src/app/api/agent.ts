c7e3ebcbf13a3044a37167939a6f0e0bad267808
Sidany Walker <sidanyk@gmail.com>
2022-08-19 11:14:28 -0400

    Use Axios interceptor
    * Use axios interceptor in agent.ts file
    * Catch all errors using interceptor in AboutPage.tsx
    * Note: Process we'll use when making API requests and catching errors

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,8 +1,15 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 axios.defaults.baseURL = 'http://localhost:5135/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    console.log('caught by interceptor')
+    return Promise.reject(error.response);
+})
 
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
